a2053aac745dafe3cf5a4c923881a944241cfb2f
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-01-02 09:26:55 +0000

    init commit

---
+++ client/main.js
@@ -0,0 +1,62 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('.chat_container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent += '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `<div class="wrapper">
+      <div class="chat">
+        <div class="profile">
+          <img
+            src="${isAi ? bot : user}"
+            alt="${isAi ? 'bot' : 'user'}"
+          />
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div>`
+  );
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+}
