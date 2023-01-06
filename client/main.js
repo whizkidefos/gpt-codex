fb3026244e8e7ce7a43f785c7b609b2bfb3f4349
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-01-06 17:45:52 +0000

    added the server app

--- client/main.js
+++ client/main.js
@@ -59,4 +59,26 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
+
+  // User's Charstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  // Bot's Chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
 }
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
